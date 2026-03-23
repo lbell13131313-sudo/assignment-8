fa5ffb08acdc2747a0af12edae3e1bad1491b3f0
lbell13131313-sudo <lbell13131313@gmail.com>
2026-03-22 20:15:34 -0400

    working on getting displayresult to work

--- js/script.js
+++ js/script.js
@@ -3,17 +3,26 @@ console.log("script.js connected!");
 //let total_points = 0;
 let points_one = 0;
 let points_two = 0;
+let points_three = 0;
+
+let points = 0;
 
 let one = document.querySelectorAll("#question-1 button");
 let two = document.querySelectorAll("#question-2 button");
+let three = document.querySelectorAll("#question-3 button");
 // console.log(buttons);
 
 points_one = question(one);
 points_two = question(two);
+points_three = question(three);
 
 
 //const total_points = point_one + point_two;
 //console.log(total_points);
+
+let result = document.getElementById("show-result");
+
+result.addEventListener("click", displayResult());
 
 function question(buttons) {
     buttons.forEach(function(button) {
@@ -24,19 +33,26 @@ function question(buttons) {
             });
 
             button.classList.add("selected");
-        
 
             let answer = button.getAttribute("data-answer");
 
             if (answer === "pop") { // plus 4 points
-                return 4;
+                points = 4;
             } else if (answer === "classical") { // plus 3 points
-                return 3;
+                points = 3;
             } else if (answer === "rock") { // plus 2 points
-                return 2;
+                points = 2;
             } else if (answer === "country") { // plus 1 point
-                return 1;
+                points = 1;
             }
         });
     });
+
+    return points;
+}
+
+function displayResult() {
+    let container = document.getElementById("result-container");
+    let total_points = points_one + points_two + points_three;
+    console.log(total_points);
 }
